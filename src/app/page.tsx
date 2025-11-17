74e3d23aedc6cbdcfd841a1676f2275f7c525fea
meatgithubyes <160728446+meatgithubyes@users.noreply.github.com>
2025-11-17 14:38:12 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,13 +26,13 @@ export default async function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Get started by editings{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.!</li>
+          <li>Save and see your changes instantly!</li>
           <li>
             ISR timestamp (updates after a regen):{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded">
